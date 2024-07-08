67f0a280df03970493e12894a9a219070c1b1f28
ddworksAt <david.do.1@ens.etsmtl.ca>
2024-07-07 21:56:33 -0400

    Moved JavaProject class to /models/java_project.py

--- backend/controllers/model_controller.py
+++ backend/controllers/model_controller.py
@@ -1,20 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel, field_validator
+from models.java_project import JavaProject
 from services.model_service import create_model
 
 router = APIRouter()
-
-# Class to define body fields for POST requests
-class JavaProject(BaseModel):
-    url: str
-
-    # Validate if field url is empty or not
-    @field_validator('url')
-    def check_url(cls, value):
-        if not value.strip():
-            # Status code 422 with relevant message
-            raise ValueError('The URL to the GitHub repository must not be empty.')
-        return value
 
 @router.post("/model/create")
 async def create_model_req(project: JavaProject):
---
+++ backend/models/java_project.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, field_validator
+
+class JavaProject(BaseModel):
+    url: str
+
+    # Validate if field url is empty or not
+    @field_validator('url')
+    def check_url(cls, value):
+        if not value.strip():
+            # Status code 422 with relevant message
+            raise ValueError('The URL to the GitHub repository must not be empty.')
+        return value
