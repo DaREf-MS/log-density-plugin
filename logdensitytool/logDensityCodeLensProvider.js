ee33baa9117f6599c7a519c17f71b3928e79d028
Royg Garcia <royggarcia@protonmail.com>
2024-07-15 10:41:55 -0400

    only show block density code lens if predicted != current

--- logdensitytool/logDensityCodeLensProvider.js
+++ logdensitytool/logDensityCodeLensProvider.js
@@ -22,7 +22,9 @@ class LogDensityCodeLensProvider {
 
     provideCodeLenses(document, token) {
         const lenses = [];
-        this.data.forEach(block => {
+        this.data
+        .filter(block => block.currentLogLevel !== block.log_level)
+        .forEach(block => {
             const position = new vscode.Position(block.blockLineStart - 1, 0);
             const range = new vscode.Range(position, position);
             const logDescription = this.log_density_classes[block.log_level] || "Unknown log density"; // Default to "Unknown" if out of bounds
@@ -31,7 +33,7 @@ class LogDensityCodeLensProvider {
             lenses.push(new vscode.CodeLens(range, {
                 command: 'extension.showLogDensityInfo',
                 title: `Predicted Log Density: ${logDescription} \u00A0 | \u00A0 Current Log Density: ${this.log_density_classes[block.currentLogLevel]}`,
-                tooltip: `Click for more details about the ${block.type} block starting at line ${block.blockLineStart} and ending at line ${block.blockLineEnd}`
+                tooltip: `Click for more details about the ${block.type} block starting at line ${block.blockLineStart} and ending at line ${block.blockLineEnd}`,
             }));
         });
         return lenses;
